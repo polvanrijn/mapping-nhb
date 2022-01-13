4f0a4d98b0dfc7126619ee5508e1148262f43983
Pol van Rijn <rijn@posteo.de>
2022-01-13 12:17:31 +0100

    Changed typo in shell script

--- src/preprocess/extract_eGeMAPS_all.sh
+++ src/preprocess/extract_eGeMAPS_all.sh
@@ -2,7 +2,7 @@ module load conda
 source activate production_MAPS
 export PATH=${PATH}:$HOME/software/opensmile/bin/
 
-for value in AHOEMO1 AHOEMO2 CaFE CREMA-D DaFEX DB_Arabic EEKK EMA EMO-DB EmoHI EmoV-DB eNTERFACE ESCAD GEMEP IITKGP_SEHSC IITKGP_SESC JUSLIN_LAUKKA_2001 MAP_HAWK MSP_improv PAX RAVDESS SAVEE TESS VENEC
+for value in AHOEMO1 AHOEMO2 CaFE CREMA-D DaFEX DB_Arabic EEKK EMA EMO-DB EmoHI EmoV-DB eNTERFACE ESCAD GEMEP IITKGP_SEHSC IITKGP_SESC JUSLIN_LAUKKA MAP_HAWK MSP_improv PAX RAVDESS SAVEE TESS VENEC
 do
     echo $value
     python extract_openSMLIE.py -i /scratch/pvanrij/corpora/${value}/preprocessed -o ../../data/${value}_eGeMAPS.csv
