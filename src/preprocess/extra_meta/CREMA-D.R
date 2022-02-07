ad673a47106d939ed72d5f02db9be9a2aa86e885
Pol van Rijn <rijn@posteo.de>
2022-02-07 15:39:29 +0100

    Syntax fix

--- src/preprocess/extra_meta/CREMA-D.R
+++ src/preprocess/extra_meta/CREMA-D.R
@@ -1,8 +1,8 @@
-df = read.csv('/Volumes/Files/Corpora/Corpora/Emotional\ corpus/Accepted/intended/CREMA-D/VideoDemographics.csv')
-speakers = row.names(df)
-df = tidyr::gather(df, new_label, value, Age:Ethnicity)[2:3]
-df$new_label = tolower(df$new_label)
-df$value = stringr::str_replace(stringr::str_replace(df$value, 'Male', 'M'), 'Female', 'F')
-df$data_grouping = 'speaker'
-df$key = speakers
+df <- read.csv('/Volumes/Files/Corpora/Corpora/Emotional\ corpus/Accepted/intended/CREMA-D/VideoDemographics.csv')
+speakers <- row.names(df)
+df <- tidyr::gather(df, new_label, value, Age:Ethnicity)[2:3]
+df$new_label <- tolower(df$new_label)
+df$value <- stringr::str_replace(stringr::str_replace(df$value, 'Male', 'M'), 'Female', 'F')
+df$data_grouping <- 'speaker'
+df$key <- speakers
 write.csv(df, 'CREMA-D.csv', row.names=F)
